032fc1cb759ce8ddaee4e0fb7c585f61fab4c7dc
HARSH JAIN <jainharsh270@gmail.com>
2020-04-11 19:35:38 +0530

    added alt text for images

--- src/Hangman.js
+++ src/Hangman.js
@@ -69,10 +69,11 @@ class Hangman extends Component {
   /** render: render game */
   render() {
     let leftGuess = this.props.maxWrong - this.state.nWrong;
+    const altText = `${this.state.nWrong}/${this.props.maxWrong}`;
     return (
       <div className="Hangman">
         <h1>Hangman</h1>
-        <img src={this.props.images[this.state.nWrong]} />
+        <img src={this.props.images[this.state.nWrong]} alt={altText} />
         {this.state.isOver ? (
           <p className="Hangman-word">{this.state.answer}</p>
         ) : (
